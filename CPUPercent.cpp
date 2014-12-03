1150a6a67d1d75c46c24365a04fbe16cdd9a8730
Chirayu Desai <chirayudesai1@gmail.com>
2014-12-03 19:50:33 +0000

    CPUPercent: Update for new API

--- CPUPercent.cpp
+++ CPUPercent.cpp
@@ -19,17 +19,22 @@ void CPU_Percent::Update()
 {
 	system_info info;
 	get_system_info(&info);
+
+	cpu_info* cpuInfos = new cpu_info[info.cpu_count];
+	get_cpu_info(0, info.cpu_count, cpuInfos);
 	
 	delta_system = (double)system_time() - last_system_time;
-	delta_cpu = double(info.cpu_infos[theCPU].active_time) - last_active_cpu_time;
+	delta_cpu = double(cpuInfos[theCPU].active_time) - last_active_cpu_time;
 	
 	percent = (delta_cpu/delta_system);
 	if(percent < 0) percent = 0.00;
 	if(percent > 1) percent = 1.00;
 	
 	lastpercent = percent;
-	last_active_cpu_time = (double)info.cpu_infos[theCPU].active_time;
+	last_active_cpu_time = (double)cpuInfos[theCPU].active_time;
 	last_system_time = (double)system_time();
+
+	delete[] cpuInfos;
 }
 
 double CPU_Percent::Percentage()
